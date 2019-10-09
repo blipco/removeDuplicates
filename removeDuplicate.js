969f2b51c3d9103caf0df4a27d0b8eeb8e4dfb5a
Bobby <blipco619@gmail.com>
2019-10-08 18:51:52 -0700

    Initial Commit

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -2,6 +2,7 @@ const input = 'AbraCadABraAlakAzam';
 
 const removeDuplicates = (input) => {
   input = input.split('');
-  input = input.reduce((acc, value) => (!RegExp(`${value}`, 'i').test(acc)) ? acc += value : acc, '')
+  input = input.reduce((acc, value) => (!RegExp(`${value}`, 'i').test(acc)) ? acc += value : acc, '');
+  console.log(input);
 }
 removeDuplicates(input)
